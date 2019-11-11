1b212ec465e1ec11e5146ffcd093c4b267d3a307
Betsy Gamrat <betsy.gamrat@wirehopper.com>
2019-11-11 16:16:02 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const Encore = require('@symfony/webpack-encore');
+const path = require('path');
+const getEzConfig = require('./ez.webpack.config.js');
+const eZConfigManager = require('./ez.webpack.config.manager.js');
+const eZConfig = getEzConfig(Encore);
+const customConfigs = require('./ez.webpack.custom.configs.js');
+
+Encore.reset();
+Encore.setOutputPath('public/assets/build')
+    .setPublicPath('/assets/build')
+    .enableSassLoader()
+    .enableReactPreset()
+    //.addStyleEntry('app-css','./assets/css/app.css')
+    //.enableVueLoader()
+    .addEntry('app', './assets/js/clock.js')
+    .enableSingleRuntimeChunk();
+
+const projectConfig = Encore.getWebpackConfig();
+
+module.exports = [ eZConfig, ...customConfigs, projectConfig ];
+
+// Put your config here.
+
+// uncomment the two lines below, if you added a new entry (by Encore.addEntry() or Encore.addStyleEntry() method) to your own Encore configuration for your project
+// const projectConfig = Encore.getWebpackConfig();
+// module.exports = [ eZConfig, ...customConfigs, projectConfig ];
+
+// comment-out this line if you've uncommented the above lines
+//module.exports = [ eZConfig, ...customConfigs ];
+
